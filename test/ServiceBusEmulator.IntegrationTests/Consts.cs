f5a43bb5fa9f2fa3d02a5c5fff5ec3a33f28ae6a
Rubén <45334651+rastrainj@users.noreply.github.com>
2024-09-25 18:25:59 +0200

    Add TestSubscriptio3Name const

--- test/ServiceBusEmulator.IntegrationTests/Consts.cs
+++ test/ServiceBusEmulator.IntegrationTests/Consts.cs
@@ -7,7 +7,8 @@
         public const string TestTopicName = "test-topic";
         public const string TestSubsciption1Name = "test-topic/Subscriptions/test-sub1";
         public const string TestSubsciption2Name = "test-topic/Subscriptions/test-sub2";
-
+        public const string TestSubsciption3Name = "test-topic/Subscriptions/test-sub3";
+        
         public const string QueueCollection = "Queue";
         public const string TopicCollection = "Topic";
 
